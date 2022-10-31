64092b670908f6d211bbbdbc9ef178cde175de60
s3864916 <s3864916@student.rmit.edu.au>
2022-10-31 11:27:28 +1100

    init project

---
+++ my-app/README.md
@@ -0,0 +1,4 @@
+This is a react project
+
+use:
+bootstrap@5.2.2
---
+++ my-app/src/fragment/CreateEmpForm.js
@@ -0,0 +1,12 @@
+import React, { useState, useContext } from "react";
+
+
+export default function CreateEmpForm() {
+
+  return (
+    <form className="form">
+        
+    </form>
+  );
+}
+
